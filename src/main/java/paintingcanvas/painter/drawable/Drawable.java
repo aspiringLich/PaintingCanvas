6e52a17177bc5d3831165693bac6a3900f45f20f
aspiringLich <sirbrandonthenerd@gmail.com>
2023-02-01 22:10:41 -0500

    animation methods (some)

--- src/main/java/paintingcanvas/painter/drawable/Drawable.java
+++ src/main/java/paintingcanvas/painter/drawable/Drawable.java
@@ -1,6 +1,9 @@
 package paintingcanvas.painter.drawable;
 
 import paintingcanvas.painter.App;
+import paintingcanvas.painter.animation.ColorAnimation;
+import paintingcanvas.painter.animation.MovementAnimation;
+import paintingcanvas.painter.animation.RotationAnimation;
 
 import java.awt.*;
 
@@ -56,11 +59,11 @@ public abstract class Drawable<T extends Drawable<T>> {
         var transform = gc.getTransform();
         var center = this.center(g);
         transform.setToRotation(this.rotation, center.x, center.y);
-        transform.scale(1.0, -1.0);
-        gc.setTransform(transform);
-
-        var bounds = gc.getClipBounds();
-        gc.translate(0.0, -bounds.getHeight());
+//        transform.scale(1.0, -1.0);
+//        gc.setTransform(transform);
+//
+//        var bounds = gc.getClipBounds();
+//        gc.translate(0.0, -bounds.getHeight());
 
         this.draw(gc);
     }
@@ -276,5 +279,103 @@ public abstract class Drawable<T extends Drawable<T>> {
      */
     public App.AnimationBuilder animate() {
         return new App.AnimationBuilder(this);
+    }
+
+    /**
+     * This method moves {@code this} to the specified {@code x} and {@code y} over {@code duration} seconds
+     *
+     * <pre>{@code
+     * Circle c = new Circle(200, 200, 50);
+     * // the circle will move to (100, 100), and then to (200, 200)
+     * c.moveTo(100, 100, 3).moveTo(200, 200, 3);
+     * }</pre>
+     *
+     * @param x        the x-position to move to
+     * @param y        the y-position to move to
+     * @param duration the number of seconds it lasts
+     * @return an {@code AnimationBuilder}
+     * @see #animate()
+     */
+    public App.AnimationBuilder moveTo(int x, int y, float duration) {
+        return this.animate().add(new MovementAnimation(0, 0, new Point(x, y), this), duration);
+    }
+
+    /**
+     * This method changes the color of {@code this} to the specified {@code color} over {@code duration} seconds.
+     * See <a href="https://en.wikipedia.org/wiki/RGB_color_model">Wikipedia</a> for how this works.
+     *
+     * <pre>{@code
+     * Circle c = new Circle(200, 200, 50);
+     * // the circle will turn red, and then blue
+     * c.colorTo(255, 0, 0, 3).colorTo(0, 0, 255, 3);
+     * }</pre>
+     *
+     * @param r        red (0-255)
+     * @param g        green (0-255)
+     * @param b        blue (0-255)
+     * @param duration the number of seconds it lasts
+     * @return an {@code AnimationBuilder}
+     * @see #animate()
+     */
+    public App.AnimationBuilder colorTo(byte r, byte g, byte b, float duration) {
+        return this.animate().add(new ColorAnimation(0, new Color(r, g, b), 0, this), duration);
+    }
+
+    /**
+     * This method changes the color of {@code this} to the specified {@code color} over {@code duration} seconds.
+     * See <a href="https://en.wikipedia.org/wiki/RGB_color_model#Numeric_representations">Wikipedia</a> for how this works.
+     *
+     * <pre>{@code
+     * Circle c = new Circle(200, 200, 50);
+     * // the circle will turn red, and then blue
+     * c.colorTo(0xFF0000, 3).colorTo(0x0000FF, 3);
+     * }</pre>
+     *
+     * @param hex      The number describing the RGB color
+     * @param duration the number of seconds it lasts
+     * @return an {@code AnimationBuilder}
+     * @see #animate()
+     */
+    public App.AnimationBuilder colorTo(int hex, float duration) {
+        return this.animate().add(new ColorAnimation(0, new Color(hex), 0, this), duration);
+    }
+
+    /**
+     * This method changes the color of {@code this} to the specified {@code color} over {@code duration} seconds.
+     * See <a href="https://docs.oracle.com/en/java/javase/17/docs/api/java.desktop/java/awt/Color.html">The Oracle docs</a> for the full list of colors,
+     * and constructors for this class
+     *
+     * <pre>{@code
+     * Circle c = new Circle(200, 200, 50);
+     * // the circle will turn red, and then blue
+     * c.colorTo(Color.RED, 3).colorTo(Color.BLUE, 3);
+     * }</pre>
+     *
+     * @param color    The color to fade to
+     * @param duration the number of seconds it lasts
+     * @return an {@code AnimationBuilder}
+     * @see #animate()
+     */
+    public App.AnimationBuilder colorTo(Color color, float duration) {
+        return this.animate().add(new ColorAnimation(0, color, 0, this), duration);
+    }
+
+    /**
+     * Creates a new rotation animation to <code>angle°</code>.
+     * If you supply an angle {@code > 360} it will make more than one full rotation.
+     *
+     * <pre>{@code
+     * Square s = new Square(200, 200, 50);
+     * // the square will rotate one turn counter-clockwise, then 2 turns clockwise
+     * c.rotateTo(360, 3).colorTo(-360, 3);
+     * }</pre>
+     *
+     * @param angle    The absolute angle to rotate to in degrees.
+     * @param duration the number of seconds it lasts
+     * @return an {@code AnimationBuilder}
+     * @see #animate()
+     */
+    public App.AnimationBuilder rotateTo(int angle, float duration) {
+        return this.animate().add(new RotationAnimation(0, 0, Math.toRadians(angle), this), duration);
     }
 }
