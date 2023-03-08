7e7bd10226d0dffe22867af9b505bfb198513ab9
Connor Slade <connor@connorcode.com>
2023-03-08 00:19:13 -0500

    Video with FFMPEG

--- src/main/java/paintingcanvas/extensions/Recorder.java
+++ src/main/java/paintingcanvas/extensions/Recorder.java
@@ -8,15 +8,53 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Recorder implements RenderLifecycle {
+    // == Screenshot ==
     final Object imgSync = new Object();
+    // == Misc ==
     boolean rendering = true;
     BufferedImage img;
+
+    // == Image Sequence ==
+    boolean recording = false;
+    int inc;
+    Path dir;
+    String format;
 
     public Recorder attach() {
         Canvas.getGlobalInstance().renderLifecycles.add(this);
         return this;
+    }
+
+    public Recorder record(String format) {
+        try {
+            this.dir = Files.createTempDirectory(".tmp");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.format = format;
+        this.recording = true;
+        this.inc = 0;
+        return this;
+    }
+
+    public Recorder stop() {
+        this.recording = false;
+        System.out.printf("Maybe: ffmpeg -framerate %d -pattern_type glob -i \"%s/tmp_*.%s\" out.mp4", Canvas.fps, this.dir, this.format);
+        return this;
+    }
+
+    public void screenshot(File file, String format) {
+        try {
+            synchronized (imgSync) {
+                ImageIO.write(img, format, file);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -31,13 +69,10 @@ public class Recorder implements RenderLifecycle {
             cmp.paint(gc);
             gc.dispose();
         }
-    }
 
-    public void screenshot(File file, String format) {
-        try {
-            ImageIO.write(img, format, file);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        if (!recording || this.dir == null) return;
+        screenshot(this.dir.resolve(Path.of(String.format("tmp_%d.%s", inc++, this.format))).toFile(), this.format);
     }
 }
+
+// ffmpeg -framerate 60 -pattern_type glob -i '*.jpg' -c:v libx264 -pix_fmt yuv420p out.mp4
--- src/test/java/examples/AnimationDebug.java
+++ src/test/java/examples/AnimationDebug.java
@@ -8,7 +8,6 @@ import paintingcanvas.extensions.FrameCounter;
 import paintingcanvas.extensions.Recorder;
 
 import java.awt.*;
-import java.io.File;
 import java.util.ArrayList;
 
 public class AnimationDebug {
@@ -20,7 +19,7 @@ public class AnimationDebug {
     public static void main(String[] argv) {
         System.setProperty("sun.java2d.opengl", "true");
         var canvas = new Canvas(width * size + 16, height * size + 16, "test");
-        var recorder = new Recorder().attach();
+        var rec = new Recorder().attach().record("jpg");
         new FrameCounter().lines(() -> new String[]{
                 String.format("Frame: %d", canvas.frame),
                 String.format("Used Memory: %dmb", (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1000 / 1000),
@@ -37,9 +36,10 @@ public class AnimationDebug {
             }
         }
 
+        var i = 0;
         while (true) {
+            if (++i >= 10) rec.stop();
             canvas.sleep(4);
-            recorder.screenshot(new File("out.png"), "png");
             var removed = new ArrayList<Rectangle>();
             for (var y = 0; y < height; y++) {
                 for (var x = 0; x < width; x++) {
