45ea394343575d25651ddbad0c59d3b6e32929a9
Connor Slade <connor@connorcode.com>
2023-09-14 00:21:50 -0400

    Add atomic function to Canvas

---
+++ src/test/java/stress_tests/AtomicDrawling.java
@@ -0,0 +1,53 @@
+package stress_tests;
+
+import paintingcanvas.canvas.Canvas;
+import paintingcanvas.drawable.Circle;
+import paintingcanvas.drawable.Polygon;
+import paintingcanvas.extensions.FrameCounter;
+import paintingcanvas.extensions.InfoDisplay;
+
+import java.awt.*;
+
+public class AtomicDrawling {
+    static Color randomColor() {
+        return new Color((float) Math.random(), (float) Math.random(), (float) Math.random());
+    }
+
+    public static void main(String[] args) {
+        Canvas canvas = new Canvas();
+        new FrameCounter().line(
+                () -> String.format("Animations: %d", canvas.animations.size())
+        ).attach();
+        new InfoDisplay().attach();
+
+        Polygon[] shapes = new Polygon[10];
+        for (int i = 0; i < shapes.length; i++) {
+            shapes[i] = new Polygon(0, 200).setColor(randomColor());
+            for (int j = 0; j < 5; j++) {
+                int x = (int) (Math.random() * 200);
+                int y = (int) (Math.random() * 200);
+                shapes[i].point(x, y);
+            }
+        }
+        while (true) {
+            int tempN = 10;
+            Circle[] temps = new Circle[tempN];
+            for (int i = 0; i < tempN; i++) {
+                temps[i] = new Circle((int) (Math.random() * canvas.getWidth()), (int) (Math.random() * canvas.getHeight()), 10, randomColor());
+            }
+
+            for (int i = 0; i < 20; i++) {
+                canvas.atomic(() -> {
+                    for (Polygon c : shapes) c.move(10, 0);
+                });
+                canvas.atomic(() -> {
+                    for (Polygon c : shapes) c.move(-10, 0);
+                });
+            }
+
+            for (int i = 0; i < tempN; i++) {
+                temps[i].erase();
+            }
+        }
+    }
+}
